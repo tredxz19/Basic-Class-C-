bfc4f008aeb50d641b334518477cb3969cca167b
Dexter Rapsing <74769892+tredxz19@users.noreply.github.com>
2022-10-12 11:01:36 +0800

    Add files via upload

---
+++ Basic Class C#.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSOLE_PRACTICE_CSHARP
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public void introduceYourself()
+        {
+            Console.WriteLine("I'm " + name + " and I'm " + age + " years old.");
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void personInfo()
+            {
+                Person personObject = new Person();
+                Console.Write("Name:");
+                personObject.name = Console.ReadLine();
+                Console.Write("Age:");
+                personObject.age = Convert.ToInt32(Console.ReadLine());
+                personObject.introduceYourself();
+                personInfo();
+                Console.Read();
+                
+            }
+            personInfo();
+        }
+    }
+}
